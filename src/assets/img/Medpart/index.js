9a437b60a78db1db86b3012b6474ce0be82a3f7e
Alfarisg 34 <alfarisg34@gmail.com>
2022-05-17 13:25:00 +0700

    asset

---
+++ src/assets/img/Medpart/index.js
@@ -0,0 +1,10 @@
+const Medpart = [
+    require('./stagejournal.png').default,
+    require('./eventpelajar.png').default,
+    require('./ruangmahasiswa.png').default,
+    require('./eventcampus.png').default,
+    require('./eventbandung.png').default,
+    require('./pissale.jpeg').default,
+]
+
+export default Medpart
