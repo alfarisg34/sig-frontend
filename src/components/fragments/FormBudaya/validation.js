69253d3866d9acdbea214a50eb4c6a8931cd9177
Alfarisg 34 <alfarisg34@gmail.com>
2022-07-08 16:09:41 +0700

    fix bug validator

--- src/components/fragments/FormBudaya/validation.js
+++ src/components/fragments/FormBudaya/validation.js
@@ -9,12 +9,12 @@ export const formBudayaSchema = yup.object().shape({
     .test({
       message: "Tidak ada foto yang dipilih",
       test: arr => arr.length>0
-    })
-    .test(
-      "fileSize", "Ukuran gambar maksimal 2MB", (value)=>{
-        return value[0] && value[0]?.size <= 2*1024*1024;
-      }
-    ),
+    }),
+    // .test(
+    //   "fileSize", "Ukuran gambar maksimal 2MB", (value)=>{
+    //     return value[0] && value[0]?.size <= 2*1024*1024;
+    //   }
+    // ),
   nama_budaya: yup.string().required('Nama Kebudayaan Wajib diisi'),
   tahun: yup.number().typeError('Tahun Belum Sesuai')
     .min(2010)
